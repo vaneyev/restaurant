eb6c1d4694b6bc7121f477e47df7df6cce4d6620
Alexander Vaneyev <vaneyev@mail.ru>
2021-01-10 10:33:05 +0300

    Refactor VoteController

--- src/main/java/org/example/restaurant/controller/VoteController.java
+++ src/main/java/org/example/restaurant/controller/VoteController.java
@@ -6,7 +6,6 @@ import org.example.restaurant.model.Vote;
 import org.example.restaurant.model.security.JpaUserDetails;
 import org.example.restaurant.repository.VoteRepository;
 import org.example.restaurant.service.DateTimeService;
-import org.example.restaurant.util.TriConsumer;
 import org.springframework.data.domain.Example;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +17,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.Optional;
+import java.util.function.BiConsumer;
 
 @RestController
 @RequestMapping("/votes")
@@ -55,16 +55,15 @@ public class VoteController {
     public ResponseEntity<?> vote(@AuthenticationPrincipal JpaUserDetails user, @PathVariable long restaurantId) {
         return getVoteResponseEntity(
                 user,
-                (oldVote, vote, dateTime) -> {
-                    if (oldVote.isPresent()) {
-                        oldVote.get().setRestaurantId(restaurantId);
-                        voteRepository.save(oldVote.get());
-                        log.info("Vote has been changed for user {}, restaurant id {}, date {}.", user.getUsername(), restaurantId, dateTime);
-                    } else {
-                        vote.setRestaurantId(restaurantId);
-                        voteRepository.save(vote);
-                        log.info("Vote has been set for user {}, restaurant id {}, date {}.", user.getUsername(), restaurantId, dateTime);
-                    }
+                (oldVote, dateTime) -> {
+                    oldVote.setRestaurantId(restaurantId);
+                    voteRepository.save(oldVote);
+                    log.info("Vote has been changed for user {}, restaurant id {}, date {}.", user.getUsername(), restaurantId, dateTime);
+                },
+                (vote, dateTime) -> {
+                    vote.setRestaurantId(restaurantId);
+                    voteRepository.save(vote);
+                    log.info("Vote has been set for user {}, restaurant id {}, date {}.", user.getUsername(), restaurantId, dateTime);
                 },
                 "Vote has not been set because current time {} is after {}");
     }
@@ -74,25 +73,30 @@ public class VoteController {
     public ResponseEntity<?> delete(@AuthenticationPrincipal JpaUserDetails user) {
         return getVoteResponseEntity(
                 user,
-                (oldVote, vote, dateTime) -> {
-                    if (oldVote.isPresent()) {
-                        voteRepository.delete(oldVote.get());
-                        log.info("Vote has been removed for user {}, date {}.", user.getUsername(), dateTime);
-                    }
+                (oldVote, dateTime) -> {
+                    voteRepository.delete(oldVote);
+                    log.info("Vote has been removed for user {}, date {}.", user.getUsername(), dateTime);
+                },
+                (vote, dateTime) -> {
                 },
                 "Vote has not been removed because current time {} is after {}");
     }
 
     private ResponseEntity<?> getVoteResponseEntity(
             JpaUserDetails user,
-            TriConsumer<Optional<Vote>, Vote, LocalDateTime> consumer,
+            BiConsumer<Vote, LocalDateTime> ifPresent,
+            BiConsumer<Vote, LocalDateTime> ifAbsent,
             String errorInfo
     ) {
         LocalDateTime dateTime = dateTimeService.getLocalDateTime();
         Vote vote = new Vote(user.getId(), dateTime.toLocalDate());
         Optional<Vote> oldVote = voteRepository.findOne(Example.of(vote));
         if (dateTime.toLocalTime().isBefore(limitTime)) {
-            consumer.accept(oldVote, vote, dateTime);
+            if (oldVote.isPresent()) {
+                ifPresent.accept(oldVote.get(), dateTime);
+            } else {
+                ifAbsent.accept(vote, dateTime);
+            }
             return ResponseEntity.noContent().build();
         }
         log.info(errorInfo, dateTime.toLocalTime(), limitTime);
--- src/main/java/org/example/restaurant/util/TriConsumer.java
+++
@@ -1,6 +0,0 @@
-package org.example.restaurant.util;
-
-@FunctionalInterface
-public interface TriConsumer<S, T, U> {
-    void accept(S s, T t, U u);
-}
